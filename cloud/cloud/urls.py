1e80cea40f561500949a5357dab93164ca474263
Chen Luo <luochen@umich.edu>
2015-04-01 09:01:50 +0800

    add sample index.html

--- cloud/cloud/urls.py
+++ cloud/cloud/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cloud.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('cloudmanufacturing.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- cloud/cloudmanufacturing/views.py
+++ cloud/cloudmanufacturing/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+	print "asdasdada"
 	return render(request, 'cloudmanufacturing/index.html')
+	#return HTTPresponse("asdasd");
